5ecac9bb78dad9870e21fe2ce198051e840f1a2a
ptucc327 <paultuccinardi@gmail.com>
2025-12-11 16:27:28 -0500

    updated files

--- DataCollection.R
+++ DataCollection.R
@@ -1,21 +1,17 @@
----
-title: "My Statistical Analysis"
-author: "Your Name"
-date: "`r Sys.Date()`"
-output: html_document
----
 
+
+# This is a good place to set global options for the document
+knitr::opts_chunk$set(echo = TRUE, warning = FALSE, message = FALSE)
 # Install packages if you don't have them
-# install.packages("R.utils")
-# install.packages("readr")
 
 # Load the necessary libraries
 library(R.utils)
 library(readr)
+library(rmarkdown)
 
 # --- Define the URL for the dataset ---
 # This is a public URL for a version of the dataset often used for teaching
-data_url <- "https://raw.githubusercontent.com/ageron/handson-ml/master/datasets/housing/housing.tgz"
+data_url <- "https://raw.githubusercontent.com/ageron/handson-ml/master/datasets/housing/housing.tgz" # nolint
 temp_file <- tempfile()
 
 # 1. Download the gzipped tarball file
